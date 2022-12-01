e509a5b39d52faac101dbff865d3ebe907b4e339
Padarat Madlee <77786747+bbellducky@users.noreply.github.com>
2022-12-02 04:26:04 +0700

    Update about

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,14 +5,14 @@ const About = () => {
         <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-600 to-black text-white'>
         
         <div className='max-w-screen-lg p-4 mx-auto flex-col justify-center w-full h-full'>
-        <img src={aboutImg} alt="3dmodel" className='mx-auto w-1/2 md:w-3/4 px-4' />
+        <img src={aboutImg} alt="3dmodel" className='mx-auto w-1/4 md:w-3/4 px-4' />
                 <div className='pb-2'>
                 <br/>
                     <p className='text-4xl font-bold flex justify-center'>About Me
                     </p>
                 </div>
-                <p className="text-xl mt-4 text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, debitis provident ut inventore tempore quia quasi blanditiis laborum, repudiandae consequuntur autem odit cumque ea voluptatibus nostrum alias nisi sed. Dignissimos.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, debitis provident ut inventore tempore quia quasi blanditiis laborum, repudiandae consequuntur autem odit cumque ea voluptatibus nostrum alias nisi sed. Dignissimos.</p>
-            </div>
+                <p className="text-xl mt-4 text-justify"> Hello, my name is Bell. I am a Junior Software Developer with highly analytical and time management skills. I am extremely good at working under pressure. I always keep up trends, I can apply my technical knowledge and skills for continuous improvement. </p>
+               </div>
         </div>
     )
 }
