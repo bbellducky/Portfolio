d82a7b6a6a411d55754b6844ece3f9173f689011
Padarat Madlee <77786747+bbellducky@users.noreply.github.com>
2022-12-02 14:44:37 +0700

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,61 +1,27 @@
-import { useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import { Link } from 'react-scroll'
-
-const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const links = [
-        {
-            id: 1,
-            link: 'home'
-        },
-        {
-            id: 2,
-            link: 'about'
-        },
-
-        {
-            id: 3,
-            link: 'portfolio'
-        },
-        {
-            id: 4,
-            link: 'skill'
-        },
-    ]
+import Landing from '../assets/Landing.png'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+const Home = () => {
     return (
-        <div className='flex justify-between items-center w-full h-20 px-4 text-gray-500  fixed '>
+        <div name="home" className='flex h-screen w-full bg-gradient-to-b from-amber-50 to-white text-black'>
+            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+                <div className='flex flex-col justify-center h-full px-8'>
+                    <h3 className='text-4xl sm:text-7xl font-bold'>I'm a  Junior Software Developer </h3>
+                    <p className='py-4 mx-w-md text-black'>Discover and let's keep in touch</p>
 
-            <div>
-                <h2 className='text-m ml-2 text-bold'>thedxbe.</h2>
+                    <div>
+                    <button className='text-black w-fit px-6 py-3 my-2 flex items-center rounded-xl border-2 border-black'>
+                        Portfolio 
+                        <span className='hover:rotate-90 duration-300'>
+                        <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+                        </span> 
+                    </button>
+                    </div>
+                </div>
+                <img src={Landing} alt="3dmodel" className='mx-auto w-1/2 md:w-3/4 px-4' />
+
             </div>
-
-            <ul className="hidden md:flex">
-                {links.map(({ id, link }) => (
-                    <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
-                        <Link to={link} smooth duration={500}>{link}
-                        </Link>
-                    </li>
-
-                ))}
-            </ul>
-
-           {/* <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500'>
-                 {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-                </div> */}
-
-            {nav && (
-                <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-yellow-50 to-white text-gray-500'>
-
-                    {links.map(({ id, link }) => (
-                        <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link to={link} smooth duration={500}>{link}
-                        </Link>
-                        </li>
-                    ))}
-                </ul>
-            )}
         </div>
     )
 }
 
-export default Navbar
+export default Home
