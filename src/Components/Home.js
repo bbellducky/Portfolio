266733a25d2bb21c0bb88b7b2f00572c9a5075fc
Padarat Madlee <77786747+bbellducky@users.noreply.github.com>
2022-12-02 14:40:31 +0700

    Edit color theme and socialside bar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,14 +2,14 @@ import Landing from '../assets/Landing.png'
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 const Home = () => {
     return (
-        <div name="home" className='flex h-screen w-full bg-gradient-to-b from-black to-gray-600 text-gray-100'>
+        <div name="home" className='flex h-screen w-full bg-gradient-to-b from-amber-50 to-white text-black'>
             <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full px-8'>
                     <h3 className='text-4xl sm:text-7xl font-bold'>I'm a  Junior Software Developer </h3>
-                    <p className='py-4 mx-w-md text-gray-200'>Discover and let's keep in touch</p>
+                    <p className='py-4 mx-w-md text-black'>Discover and let's keep in touch</p>
 
                     <div>
-                    <button className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-xl bg-gradient-to-r from-cyan-500 to-blue-500'>
+                    <button className='text-black w-fit px-6 py-3 my-2 flex items-center rounded-xl border-2 border-black'>
                         Portfolio 
                         <span className='hover:rotate-90 duration-300'>
                         <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
