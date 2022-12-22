a33281a1c3942f14197c8103327e10e969656705
Padarat Madlee <77786747+bbellducky@users.noreply.github.com>
2022-12-22 18:31:19 +0700

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -10,7 +10,7 @@ const Portfolio = () => {
         },
     ]
     return (
-        <div name="portfolio" className='bg-gradient-to-b from-amber-50 to-white w-full text-black md:h-screen'>
+        <div name="portfolio" className='bg-gradient-to-b from-white to-amber-50 w-full text-black md:h-screen'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline '>Portfolio</p>
