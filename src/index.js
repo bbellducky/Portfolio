404cc2c8437f92299033e812f4cabd08692e888b
Padarat Madlee <77786747+bbellducky@users.noreply.github.com>
2022-12-22 18:53:04 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<link rel="icon" type="image/x-icon" href="/assets/b.svg">
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
